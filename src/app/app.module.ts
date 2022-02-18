24bf180e8941e73b87308416f56cf521fdbf4d08
Ian3128 <ianhuntink@hotmail.com>
2022-02-18 11:45:18 +0100

    Spelletjes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { TictactoeComponent } from './games/tictactoe/tictactoe.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TictactoeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
---
+++ src/app/games/tictactoe/tictactoe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tictactoe',
+  templateUrl: './tictactoe.component.html',
+  styleUrls: ['./tictactoe.component.css']
+})
+export class TictactoeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
